e01b1f834c41dd3f73d1ea401136ac6ec50691a3
Kevin Zhang <zhangjk8@yahoo.com>
2016-05-26 13:01:25 -0400

    First commit

--- cv_char_script.py
+++ cv_char_script.py
@@ -4,13 +4,12 @@ import sys
 import scipy as sp
 from scipy import stats
 
-def find_streak(cap_vs_volt, start_index, rmse_threshold):
-    slope, intercept, r_value, streak_size = 0,0,0,0
+
+def find_streak(cap_vs_volt, start_index, p_threshold):
+    slope, intercept, p_value, streak_size = 0,0,0,0
     for end_index in range(start_index+3, cap_vs_volt.shape[0]):
-        slope, intercept, r_value, _, _ = sp.stats.linregress(cap_vs_volt[start_index:end_index, :])
-        pred = slope * cap_vs_volt[start_index:end_index, 0] + intercept
-        rmse = np.linalg.norm(pred - cap_vs_volt[start_index:end_index, 1]) / np.sqrt(end_index - start_index)
-        if rmse > rmse_threshold:
+        slope, intercept, _, p_value, _ = sp.stats.linregress(cap_vs_volt[start_index:end_index, :])
+        if p_value > p_threshold:
             streak_size = end_index - start_index - 1
             break
     streak_dist = np.linalg.norm(cap_vs_volt[start_index, :] - cap_vs_volt[start_index+streak_size, :])
@@ -20,35 +19,35 @@ def find_streak(cap_vs_volt, start_index, rmse_threshold):
         x_intercept = None
     return streak_size, streak_dist, slope, x_intercept
 
-def find_idealCV_line_generator(cap_vs_volt):
-    energy_bandgap = 1.5
-    rmse_threshold = .25*np.median(cap_vs_volt[:,1])
-    size_threshold = 5
-    initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
-    longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
-    for start_index in range(cap_vs_volt.shape[0]):
-        yield find_streak(cap_vs_volt, start_index, rmse_threshold)
-
-
-# def find_idealCV_line_OG(cap_vs_volt):
+# def find_idealCV_line_generator(cap_vs_volt):
 #     energy_bandgap = 1.5
-#     rmse_threshold = .1*np.median(cap_vs_volt[:,1])
+#     p_threshold = .01
 #     size_threshold = 5
 #     initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
 #     longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
 #     for start_index in range(cap_vs_volt.shape[0]):
-#         current_streak_size, current_streak_dist, current_slope, current_x_intercept = find_streak(cap_vs_volt, start_index, rmse_threshold)
-#         # maybe threshold for how much more negative the slope should be
-#         if (current_slope < min(0, initial_slope)) and \
-#             (current_x_intercept < energy_bandgap) and \
-#             (current_streak_dist > longest_streak_dist) and \
-#             (current_streak_size > size_threshold):
-#
-#             longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = current_slope,\
-#                                                                                     current_streak_dist,\
-#                                                                                     current_x_intercept
-#
-#     return longest_streak_slope, longest_streak_x_intercept
+#         yield find_streak(cap_vs_volt, start_index, p_threshold)
+
+
+def find_idealCV_line(cap_vs_volt):
+    energy_bandgap = 1.5
+    p_threshold = .01
+    size_threshold = 0
+    initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
+    longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
+    for start_index in range(cap_vs_volt.shape[0]):
+        current_streak_size, current_streak_dist, current_slope, current_x_intercept = find_streak(cap_vs_volt, start_index, p_threshold)
+        # maybe threshold for how much more negative the slope should be
+        if (current_slope < min(0, initial_slope)) and \
+            (current_x_intercept < energy_bandgap) and \
+            (current_streak_dist > longest_streak_dist) and \
+            (current_streak_size > size_threshold):
+
+            longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = current_slope,\
+                                                                                    current_streak_dist,\
+                                                                                    current_x_intercept
+
+    return longest_streak_slope, longest_streak_x_intercept
 
 
 
@@ -70,19 +69,38 @@ def main(argv):
         x = cap_vs_volt_forward[:, 0]
         y = cap_vs_volt_forward[:,1]
         fig, ax = plt.subplots()
-        ax.plot(x, y,'.', markersize = 25,)
-        for size, dist, slope, x_intercept in find_idealCV_line_generator(cap_vs_volt_forward):
-            if x_intercept is None or slope > 0:
-                continue
-            ax.plot(x, slope * x + x_intercept * (-slope), '-')
+        ax.plot(x, y,'.', markersize = 2,)
+        ideal_slope, ideal_x_intercept = find_idealCV_line(cap_vs_volt_forward)
+        # ax.plot(x, ideal_slope * x + ideal_x_intercept * (-ideal_slope), '-')
+        print("Slope = ", ideal_slope)
+        print("X_Intercept = ", ideal_x_intercept)
         plt.show()
 
 
-        # best_fit_line_slope, best_fit_line_x_intercept = find_idealCV_line(cap_vs_volt_forward)
-        # print("Slope = ", best_fit_line_slope)
-        # print("X_Intercept = ", best_fit_line_x_intercept)
+# def main(argv):
+    # directoryname = argv[0]
 
-
+    # for file_number in range(200, 205, 5):
+    #     filename = "{0}dev2_T{1}K_F100000HZ_CV.txt".format(directoryname, file_number)
+    #     try:
+    #         cv_raw = np.genfromtxt(filename, delimiter=',', skip_header=1, usecols=(0,1))
+    #     except IOError:
+    #         continue
+    #     capacitances = cv_raw[:,1]
+    #     cap_inverse_square = np.reciprocal(np.square(capacitances))
+    #     cap_vs_volt = np.column_stack((cv_raw[:,0], cap_inverse_square))
+    #     print(cap_vs_volt)
+    #     cap_vs_volt_forward = cap_vs_volt[:101, :]
+    #     cap_vs_volt_reverse = cap_vs_volt[101:,:]
+    #     x = cap_vs_volt_forward[:, 0]
+    #     y = cap_vs_volt_forward[:,1]
+    #     fig, ax = plt.subplots()
+    #     ax.plot(x, y,'.', markersize = 25,)
+    #     for size, dist, slope, x_intercept in find_idealCV_line_generator(cap_vs_volt_forward):
+    #         if x_intercept is None or slope > 0:
+    #             continue
+    #         ax.plot(x, slope * x + x_intercept * (-slope), '-')
+    #     plt.show()
 
 
 
--- cv_char_script_rmse.py
+++ cv_char_script_rmse.py
@@ -4,13 +4,13 @@ import sys
 import scipy as sp
 from scipy import stats
 
-def find_streak(cap_vs_volt, start_index, rmse_threshold):
+def find_streak(cap_vs_volt, start_index, diff_threshold):
     slope, intercept, r_value, streak_size = 0,0,0,0
     for end_index in range(start_index+3, cap_vs_volt.shape[0]):
         slope, intercept, r_value, _, _ = sp.stats.linregress(cap_vs_volt[start_index:end_index, :])
         pred = slope * cap_vs_volt[start_index:end_index, 0] + intercept
-        rmse = np.linalg.norm(pred - cap_vs_volt[start_index:end_index, 1]) / np.sqrt(end_index - start_index)
-        if rmse > rmse_threshold:
+        diff = np.max(np.abs(pred - cap_vs_volt[start_index:end_index, 1]))
+        if diff > diff_threshold:
             streak_size = end_index - start_index - 1
             break
     streak_dist = np.linalg.norm(cap_vs_volt[start_index, :] - cap_vs_volt[start_index+streak_size, :])
@@ -20,42 +20,39 @@ def find_streak(cap_vs_volt, start_index, rmse_threshold):
         x_intercept = None
     return streak_size, streak_dist, slope, x_intercept
 
-def find_idealCV_line_generator(cap_vs_volt):
-    energy_bandgap = 1.5
-    rmse_threshold = .25*np.median(cap_vs_volt[:,1])
-    size_threshold = 5
-    initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
-    longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
-    for start_index in range(cap_vs_volt.shape[0]):
-        yield find_streak(cap_vs_volt, start_index, rmse_threshold)
-
-
-# def find_idealCV_line_OG(cap_vs_volt):
+# def find_idealCV_line_generator(cap_vs_volt):
 #     energy_bandgap = 1.5
-#     rmse_threshold = .1*np.median(cap_vs_volt[:,1])
+#     diff_threshold = .001*np.median(cap_vs_volt[:,1])
 #     size_threshold = 5
 #     initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
 #     longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
 #     for start_index in range(cap_vs_volt.shape[0]):
-#         current_streak_size, current_streak_dist, current_slope, current_x_intercept = find_streak(cap_vs_volt, start_index, rmse_threshold)
-#         # maybe threshold for how much more negative the slope should be
-#         if (current_slope < min(0, initial_slope)) and \
-#             (current_x_intercept < energy_bandgap) and \
-#             (current_streak_dist > longest_streak_dist) and \
-#             (current_streak_size > size_threshold):
-#
-#             longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = current_slope,\
-#                                                                                     current_streak_dist,\
-#                                                                                     current_x_intercept
-#
-#     return longest_streak_slope, longest_streak_x_intercept
+#         yield find_streak(cap_vs_volt, start_index, diff_threshold)
 
 
+def find_idealCV_line(cap_vs_volt):
+    energy_bandgap = 15
+    diff_threshold = .001*np.median(cap_vs_volt[:,1])
+    size_threshold = 0
+    initial_slope, _, _, _, _ = sp.stats.linregress(cap_vs_volt[:10, :])
+    longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = 0,0,0
+    for start_index in range(cap_vs_volt.shape[0]):
+        current_streak_size, current_streak_dist, current_slope, current_x_intercept = find_streak(cap_vs_volt, start_index, diff_threshold)
+        if (current_slope < min(0, initial_slope)) and \
+            (current_x_intercept < energy_bandgap) and \
+            (current_streak_dist > longest_streak_dist) and \
+            (current_streak_size > size_threshold):
+
+            longest_streak_slope, longest_streak_dist, longest_streak_x_intercept = current_slope,\
+                                                                                    current_streak_dist,\
+                                                                                    current_x_intercept
+
+    return longest_streak_slope, longest_streak_x_intercept
 
 def main(argv):
-    directoryname = argv[0]
 
-    for file_number in range(200, 205, 5):
+    directoryname = argv[0]
+    for file_number in range(310, 315, 5):
         filename = "{0}dev2_T{1}K_F100000HZ_CV.txt".format(directoryname, file_number)
         try:
             cv_raw = np.genfromtxt(filename, delimiter=',', skip_header=1, usecols=(0,1))
@@ -70,13 +67,42 @@ def main(argv):
         x = cap_vs_volt_forward[:, 0]
         y = cap_vs_volt_forward[:,1]
         fig, ax = plt.subplots()
-        ax.plot(x, y,'.', markersize = 25,)
-        for size, dist, slope, x_intercept in find_idealCV_line_generator(cap_vs_volt_forward):
-            if x_intercept is None or slope > 0:
-                continue
-            ax.plot(x, slope * x + x_intercept * (-slope), '-')
+        axes = plt.gca()
+        axes.set_ylim([.99*np.min(cap_vs_volt_forward[:,1]), 1.01*np.max(cap_vs_volt_forward[:,1])])
+        ax.plot(x, y,'.', markersize = 10,)
+        ideal_slope, ideal_x_intercept = find_idealCV_line(cap_vs_volt_forward)
+        print("Slope = ", ideal_slope)
+        print("X_Intercept = ", ideal_x_intercept)
+        ax.plot(x, ideal_slope * x + ideal_x_intercept * (-ideal_slope), '-')
         plt.show()
 
+
+# def main(argv):
+#     directoryname = argv[0]
+#
+#     for file_number in range(200, 205, 5):
+#         filename = "{0}dev2_T{1}K_F100000HZ_CV.txt".format(directoryname, file_number)
+#         try:
+#             cv_raw = np.genfromtxt(filename, delimiter=',', skip_header=1, usecols=(0,1))
+#         except IOError:
+#             continue
+#         capacitances = cv_raw[:,1]
+#         cap_inverse_square = np.reciprocal(np.square(capacitances))
+#         cap_vs_volt = np.column_stack((cv_raw[:,0], cap_inverse_square))
+#         print(cap_vs_volt)
+#         cap_vs_volt_forward = cap_vs_volt[:101, :]
+#         cap_vs_volt_reverse = cap_vs_volt[101:,:]
+#         x = cap_vs_volt_forward[:, 0]
+#         y = cap_vs_volt_forward[:,1]
+#         fig, ax = plt.subplots()
+#         axes = plt.gca()
+#         axes.set_ylim([.99*np.min(cap_vs_volt_forward[:,1]), 1.01*np.max(cap_vs_volt_forward[:,1])])
+#         ax.plot(x, y,'.', markersize = 25,)
+#         for size, dist, slope, x_intercept in find_idealCV_line_generator(cap_vs_volt_forward):
+#             if x_intercept is None or slope > 0:
+#                 continue
+#             ax.plot(x, slope * x + x_intercept * (-slope), '-')
+#         plt.show()
 
         # best_fit_line_slope, best_fit_line_x_intercept = find_idealCV_line(cap_vs_volt_forward)
         # print("Slope = ", best_fit_line_slope)
